5432595f0754d6674bb86e6e9bc82b4e6104e785
Ivchenko Oleg <korvin2985@gmail.com>
2023-09-02 22:51:31 +0300

    refactoring

--- Open_email_ukr_net.py
+++ Open_email_ukr_net.py
@@ -6,9 +6,6 @@ import base64
 
 
 def open_email():
-
-
-
     imap_server = "imap.ukr.net"
     imap = imaplib.IMAP4_SSL(imap_server)
 
@@ -28,28 +25,23 @@ def open_email():
     #text_subject = imap.fetch(b'13', "(BODY[HEADER.FIELDS (Subject)])")
     #print(text_subject)
 
-    text_email = msg.get_payload()
-
-    #print(text_email)
     ref = ''
     for part in msg.walk():
         if part.get_content_maintype() == 'text' and part.get_content_subtype() == 'plain':
-           # print(part.get_payload())
-            message = (base64.b64decode(part.get_payload()).decode())
-           # message = part.get_payload.decode()
-            #charset = part.get_charset()
-            #message = str(part.get_payload(decode=True), str(charset), 'ignore').encode('utf8', 'replace')
-           # print(message)
-
-            #for i in range (0,len(part.get_payload())-2):
-             #   ref = ref + part.get_payload()[i]
-            #print(ref)
-            #return(str(ref))
+           message = (base64.b64decode(part.get_payload()).decode())
+           #message = part.get_payload.decode()
+           #charset = part.get_charset()
+           #message = str(part.get_payload(decode=True), str(charset), 'ignore').encode('utf8', 'replace')
+           #print(message)
+           #for i in range (0,len(part.get_payload())-2):
+           #ref = ref + part.get_payload()[i]
+           #print(ref)
+           #return(str(ref))
     message_code = message.split(":")[1].rsplit()
     #print(message.split(":")[1])
     #print(message_code[0])
 
-    return (message_code[0])                                                                #print(base64.b64decode(part.get_payload()).decode())
+    return (message_code[0])
 
 
 open_email()
